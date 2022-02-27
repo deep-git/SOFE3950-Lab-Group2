46ed4d97098df58538c49fcd6a33e3f31c36318a
rnlclngpilar <rnlclng.pilar@gmail.com>
2022-02-26 22:49:13 -0500

    added shell/welcome prompt

--- Lab2-Shell/myshell.c
+++ Lab2-Shell/myshell.c
@@ -2,4 +2,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <myshell.h>
+#include "myshell.h"
+
+void initialize(void){
+
+    system("clear");
+    //system("pwd");
+
+    // prompt user with welcome msg
+    printf("\n****************************************************");
+    printf("\n      This is a Shell/Command Line Interpreter");
+    printf("\n                  Written in C");
+    printf("\n****************************************************\n");
+
+}
+
+void shellPrompt(void){ 
+    printf("~/myShell$ ");
+}
+
+int main (int argc, char *argv[]){
+
+    // call function
+    initialize();   
+    
+    while(1){
+        shellPrompt();
+        getline(&input, &len, stdin); // Read the user input
+        printf(input);
+
+    }
+}
