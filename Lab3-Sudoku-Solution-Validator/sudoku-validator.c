27b3f79a44e71900d75d615f996578285c38f87d
deep-git <57687837+deep-git@users.noreply.github.com>
2022-03-17 19:38:42 -0400

    Update functions and add valid detector array

--- Lab3-Sudoku-Solution-Validator/sudoku-validator.c
+++ Lab3-Sudoku-Solution-Validator/sudoku-validator.c
@@ -3,10 +3,11 @@
 #include <string.h>
 #include <pthread.h>
 
-typedef struct {
+#define COUNT 9
+
+typedef struct grid {
     int row;
     int column;
-    int (* grid_numbers)[9];
 } parameters;
 
 /*
@@ -23,40 +24,18 @@ static int sudoku_board[9][9] = {
                             };
 */
 
-int * count_rows(void * data);
-int * count_columns(void * data);
+void * count_rows(void * data);
+void * count_columns(void * data);
 void * validate_number(void * data);
 
+int grid_numbers[9][9];
+int valid[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 int main (void) {
 
   FILE *file; // Declaring a file pointer
-  //char buffer[1024];
-  int grid_numbers[9][9];
 
   file = fopen("Lab3 puzzle.txt", "r"); // Opening the file with read permissions
-
-  //fgets(buffer, sizeof(buffer), fp); // Reading from file into the buffer variable
-  /*
-  if (file == NULL) {
-      printf("Error opening the file");
-      return 1;
-  }
-
-  int line_num = 0;
-  char c = 0;
-
-  c = fgetc(file);
-
-  while (c != EOF) {
-
-      printf("%c", c);
-      c = fgetc(file);
-
-  }
-
-  fclose(file);
-  */
 
   if (file != NULL) {
     for (int i = 0; i < 9; i++) {
@@ -118,17 +97,17 @@ printf("\n");
     // Create threads
     pthread_t t_row, t_col, t_1, t_2, t_3, t_4, t_5, t_6, t_7, t_8, t_9;
 
-    pthread_create(&t_row, NULL, count_rows, (void *) data0);
-    pthread_create(&t_col, NULL, count_columns, (void *) data0);
-    pthread_create(&t_1, NULL, validate_number, (void *) data1);
-    pthread_create(&t_2, NULL, validate_number, (void *) data2);
-    pthread_create(&t_3, NULL, validate_number, (void *) data3);
-    pthread_create(&t_4, NULL, validate_number, (void *) data4);
-    pthread_create(&t_5, NULL, validate_number, (void *) data5);
-    pthread_create(&t_6, NULL, validate_number, (void *) data6);
-    pthread_create(&t_7, NULL, validate_number, (void *) data7);
-    pthread_create(&t_8, NULL, validate_number, (void *) data8);
-    pthread_create(&t_9, NULL, validate_number, (void *) data9);
+    pthread_create(&t_row, NULL, count_rows, data0);
+    pthread_create(&t_col, NULL, count_columns, data0);
+    pthread_create(&t_1, NULL, validate_number, data1);
+    pthread_create(&t_2, NULL, validate_number, data2);
+    pthread_create(&t_3, NULL, validate_number, data3);
+    pthread_create(&t_4, NULL, validate_number, data4);
+    pthread_create(&t_5, NULL, validate_number, data5);
+    pthread_create(&t_6, NULL, validate_number, data6);
+    pthread_create(&t_7, NULL, validate_number, data7);
+    pthread_create(&t_8, NULL, validate_number, data8);
+    pthread_create(&t_9, NULL, validate_number, data9);
 
     void * rows;
     void * columns;
@@ -154,56 +133,112 @@ printf("\n");
     pthread_join(t_8, &grid8);
     pthread_join(t_9, &grid9);
 
+    /*
     if ((int) rows == 1 && (int) columns == 1 && (int) grid1 == 1 && (int) grid2 == 1 && (int) grid3 == 1 && (int) grid4 == 1 && (int) grid5 == 1 && (int) grid6 == 1 &&
     (int) grid7 == 1 && (int) grid8 == 1 && (int) grid9 == 1) {
         printf("The sudoku puzzle was solved successfully, congratulations!\n");
     } else {
         printf("The sudoku puzzle is not solved correctly, please try again\n");
     }
+    */
+
+    for (int i = 0; i < 11; i++) {
+      if (valid[i] == 0) {
+        printf("The sudoku puzzle is not solved correctly, please try again\n");
+        return 0;
+      }
+    }
+
+    printf("The sudoku puzzle was solved successfully, congratulations!\n");
 
     return 0;
 }
 
 // Checks whether or not the rows contain numbers 1-9, if they do, then return 1
-int * count_rows(void * data) {
+void * count_rows(void * data) {
 
-    /*
-    for (int i = 0; i < 9; i++) {
-        if (sudoku_board[rows][i] < 0 || sudoku_board[rows][i] > 0) {
-            return -1;
-        } else {
-            if (sudoku_board[rows][i] != 0) {
+  parameters *data_entry = (parameters *) data;
+  int count_rows = data_entry->row;
+  int count_columns = data_entry->column;
+  int valid_row[COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-            }
-        }
+  for (int i = count_rows; i < COUNT; i++) {
+
+    for (int j = count_columns; j < COUNT; i++) {
+
+      int num_valid = grid_numbers[i][j];
+
+      if (num_valid < 0 || num_valid > 9 || valid_row[num_valid - 1] == 1) {
+        pthread_exit(NULL);
+
+      } else {
+        valid_row[num_valid - 1] = 1;
+      }
+
     }
-    */
+  }
+
+  valid[0] = 1;
+
+  pthread_exit(NULL);
 }
 
 // Checks whether or not the columns contain numbers 1-9, if they do, then return 1
-int * count_columns(void * data) {
+void * count_columns(void * data) {
 
-    /*
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j <= 9; j++) {
-            int count = 0;
-            for (int k = 0; k < 9; k++) {
-                if (sudoku_board[k][i] == j) {
-                    if (count > 0) {
-                        return 0;
-                    }
-                    count = 1;
-                }
-            }
-        }
+  parameters *data_entry = (parameters *) data;
+  int count_rows = data_entry->row;
+  int count_columns = data_entry->column;
+  int valid_columns[COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+  for (int i = count_columns; i < COUNT; i++) {
+
+    for (int j = count_rows; j < COUNT; i++) {
+
+      int num_valid = grid_numbers[j][i];
+
+      if (num_valid < 0 || num_valid > 9 || valid_columns[num_valid - 1] == 1) {
+        pthread_exit(NULL);
+
+      } else {
+        valid_columns[num_valid - 1] = 1;
+      }
+
     }
+  }
 
-    return 1;
-    */
+  valid[1] = 1;
 
+  pthread_exit(NULL);
 }
 
 // Checks whether or not each of the 9 grids contain numbers 1-9, if they do, then return 1
 void * validate_number(void * data) {
 
+  parameters *data_entry = (parameters *) data;
+  int count_rows = data_entry->row;
+  int count_columns = data_entry->column;
+  int valid_grid[COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+  for (int i = count_rows; i < count_rows + 3; i++) {
+
+    for (int j = count_columns; j < count_columns + 3; j++) {
+
+      int num_valid = grid_numbers[i][j];
+
+      if (num_valid < 0 || num_valid > 9 || valid_grid[num_valid - 1] == 1) {
+        pthread_exit(NULL);
+
+      } else {
+        valid_grid[num_valid - 1] = 1;
+      }
+
+    }
+  }
+
+  for (int i = 2; i < COUNT; i++) {
+      valid[i] = 1;
+  }
+
+  pthread_exit(NULL);
 }
