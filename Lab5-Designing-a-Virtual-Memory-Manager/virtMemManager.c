3efab4085ece0d4c71425903caec473be5795e70
rnlclngpilar <rnlclng.pilar@gmail.com>
2022-04-15 17:07:39 -0400

    added open file

--- Lab5-Designing-a-Virtual-Memory-Manager/virtMemManager.c
+++ Lab5-Designing-a-Virtual-Memory-Manager/virtMemManager.c
@@ -4,5 +4,16 @@
 
 int main(int argc, char* argv[]){
 
+    if(argc < 2){
+        printf("--------------------------------------\n");
+        printf("USAGE: ./virtMemManager.o <filename>\n");
+        printf("--------------------------------------\n");
+        exit(0);
+    }
 
+    FILE *f = fopen(argv[1], "r");
+
+
+
+    
 }
